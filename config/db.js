f7d1478b672b2925a048eea5e1221996ed575ec8
Pradipta Mandal <pradiptamandal789@gmail.com>
2023-03-23 19:01:33 +0530

    Added Node API with JWT Auth

---
+++ config/db.js
@@ -0,0 +1,5 @@
+module.exports ={
+    'secret' : "10xauthsecret",
+    //'db':"mongodb://localhost:27017/node-auth"
+    'db':'mongodb+srv://pradiptamandal789:<password>@cluster0.67glxv0.mongodb.net/?retryWrites=true&w=majority'
+}
